2403d749e5bff36427e13c547c91ec252a599aae
Alexandre Geissmann <alex@la-bonaventure.com>
2020-06-24 15:24:29 -0400

    initial

---
+++ packages/app/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="maincolor">I'm styled with a library inside a workspace</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+<style lang="postcss">
+@import "csslib";
+</style>
